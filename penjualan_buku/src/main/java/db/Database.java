918a22b1bd87903b04ae8403b86c1dd163120e12
Vicco Dwi <50991950+vicco7s@users.noreply.github.com>
2022-01-01 16:45:06 +0800

    Login with preference

--- penjualan_buku/src/main/java/db/Database.java
+++ penjualan_buku/src/main/java/db/Database.java
@@ -4,10 +4,28 @@
  */
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author cero
  */
 public class Database {
+    private final String URL = "jdbc:mysql://localhost:3306/";
+    private final String DB_NAME = "penjualan_buka";
+    private final String USER = "root";
+    private final String PASS = "";
     
+    public Connection getConnection(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(URL+DB_NAME, USER, PASS);
+            System.out.println("Koneksi Berhasil");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex.toString());
+        }
+        return con;
+    }
 }
